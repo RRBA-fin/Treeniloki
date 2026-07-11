7af35d10b05ac2ff6fde004eb2b5328f272728d5
RRBA-fin <niko.rintamaki4@gmail.com>
2026-07-11 10:18:27 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Treeniloki service worker — nosta versionumeroa aina kun index.html päivittyy! */
-const CACHE = "treeniloki-v1";
+const CACHE = "treeniloki-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
